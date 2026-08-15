71527ea38516e1bfc09a40fc249fabd231096aea
matuskocis0000-del <matuskocis0000@gmail.com>
2026-08-15 12:51:17 +0200

    Refactor RageMenuPopup to use FLAlertLayer
    Removed custom RageMenuPopup class and replaced its functionality with a simple alert layer.

--- src_main.cpp
+++ src_main.cpp
@@ -3,49 +3,10 @@
 
 using namespace geode::prelude;
 
-// Definícia vlastného popup okna
-class RageMenuPopup : public GeodeSquarePopup {
-protected:
-    bool setup() override {
-        this->setTitle("Rage Menu");
-
-        auto winSize = CCDirector::sharedDirector()->getWinSize();
-        
-        // Pridanie textu do menu
-        auto label = CCLabelBMFont::create("Vitaj v Rage Mode!", "bigFont.fnt");
-        label->setScale(0.5f);
-        label->setPosition(m_mainLayer->getContentSize().width / 2, m_mainLayer->getContentSize().height / 2 + 30);
-        m_mainLayer->addChild(label);
-
-        // Pokus o načítanie obrázka zo zložky resources
-        auto sprite = CCSprite::create("resources/rage-menu.png");
-        if (sprite) {
-            sprite->setPosition(m_mainLayer->getContentSize() / 2 - ccp(0, 20));
-            sprite->setScale(0.5f);
-            m_mainLayer->addChild(sprite);
-        }
-
-        return true;
-    }
-
-public:
-    static RageMenuPopup* create() {
-        auto ret = new RageMenuPopup();
-        if (ret && ret->init(240.f, 180.f)) {
-            ret->autorelease();
-            return ret;
-        }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-};
-
-// Modifikácia hlavného menu hry
 class $modify(MenuLayer) {
     bool init() override {
         if (!MenuLayer::init()) return false;
 
-        // Načítanie hlavnej ikony módu
         auto buttonSprite = CCSprite::create("logo.png");
         if (!buttonSprite) {
             buttonSprite = CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
@@ -57,7 +18,6 @@ class $modify(MenuLayer) {
             menu_selector(MenuLayer::onRageMenu)
         );
 
-        // Pridanie tlačidla do spodného riadku menu
         auto bottomMenu = this->getChildByID("bottom-menu");
         if (bottomMenu) {
             bottomMenu->addChild(rMenuBtn);
@@ -68,8 +28,6 @@ class $modify(MenuLayer) {
     }
 
     void onRageMenu(CCObject* sender) {
-        if (auto popup = RageMenuPopup::create()) {
-            popup->show();
-        }
+        FLAlertLayer::create("Rage Menu", "Modifikacia bola uspesne nacitana!", "OK")->show();
     }
 };
